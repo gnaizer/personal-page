e9d13a08949f11a43b4b8fbfbed3b8ae65a39983
Nazar <gntnaizer@gmail.com>
2017-10-06 15:24:24 +0000

    Final version

--- js/map.js
+++ js/map.js
@@ -1,19 +1,4 @@
-// var map;
-//       function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           center: {lat: 49.811543, lng:  24.036122},
-//           zoom: 8
-//         });
-//         function toggleBounce() {
-//         if (marker.getAnimation() !== null) {
-//             marker.setAnimation(null);
-//         } else {
-//             marker.setAnimation(google.maps.Animation.BOUNCE);
-//         }    
-//     }
-//       }
-
-   function initMap() {
+ function initMap() {
         var uluru = {lat: 49.811556, lng: 24.036111};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
